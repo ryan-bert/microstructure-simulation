599f7dc12b1c4be081542c749b6eb664e7026685
ryan-bert <ryanberti66@gmail.com>
2025-06-12 20:36:55 +0200

    Implemented basic Order class

---
+++ include/Order.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+
+enum class OrderType { Market, Limit };
+enum class Side { Buy, Sell };
+
+class Order {
+public:
+    Order(int id, Side side, double price, int quantity, OrderType type);
+
+    int getId() const;
+    Side getSide() const;
+    double getPrice() const;
+    int getQuantity() const;
+    OrderType getType() const;
+
+private:
+    int id;
+    Side side;
+    double price;
+    int quantity;
+    OrderType type;
+};
