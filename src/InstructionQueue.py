89a9df76fbfbc08c7cc64f4a520a893e47e60557
ryan-bert <ryanberti66@gmail.com>
2025-06-14 19:43:11 +0200

    created process_next_instruction() method

--- src/InstructionQueue.py
+++ src/InstructionQueue.py
@@ -25,4 +25,24 @@ class InstructionQueue:
         self.active_instructions = deque()
         self.processed_instructions = deque()
 
-    
+
+    # Method to add an instruction to the queue
+    def add_instruction(self, instruction):
+        self.active_instructions.append(instruction)
+
+
+    # Method to process the next instruction in the queue
+    def process_next_instruction(self):
+
+        # Check if there are any active instructions
+        if not self.active_instructions:
+            return None
+        
+        # Pop the next instruction from the queue
+        instruction = self.active_instructions.popleft()
+
+        # Record the time and mark as processed
+        instruction.processed_timestamp = time()
+        self.processed_instructions.append(instruction)
+
+        return instruction
