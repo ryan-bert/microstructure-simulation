a1e42ef8e37ea02860e2e0f71c33f50157e5dbe1
ryan-bert <ryanberti66@gmail.com>
2025-06-14 22:01:14 +0200

    Constructor for matching engine

--- src/MatchingEngine.py
+++ src/MatchingEngine.py
@@ -1,3 +1,13 @@
 from OrderBook import OrderBook
 from Order import Order, OrderType, Side
 from InstructionQueue import Instruction, InstructionType, InstructionQueue
+from ExecutedTrades import Trade, ExecutedTrades
+
+class MatchingEngine:
+
+    # Constructor for the MatchingEngine class
+    def __init__(self, order_book, instruction_queue, executed_trades):
+        self.order_book = order_book
+        self.instruction_queue = instruction_queue
+        self.executed_trades = executed_trades
+
