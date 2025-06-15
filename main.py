eadda32819ff100dd45ec226dd93b9271becbebd
ryan-bert <ryanberti66@gmail.com>
2025-06-15 22:12:38 +0200

    Converted to singe to_instruction() method

--- main.py
+++ main.py
@@ -10,7 +10,20 @@ from src.Order import Order, OrderType, Side
 def main():
     
     # Create market for SPY
-    
+    spy_market = Market("SPY")
+
+    # Create some liquidity
+    order1 = Order(order_id=1, side=Side.BUY, quantity=10, order_type=OrderType.LIMIT, price=101)
+    order2 = Order(order_id=2, side=Side.BUY, quantity=15, order_type=OrderType.LIMIT, price=100)
+    order3 = Order(order_id=3, side=Side.BUY, quantity=20, order_type=OrderType.LIMIT, price=99)
+    order4 = Order(order_id=4, side=Side.SELL, quantity=10, order_type=OrderType.LIMIT, price=102)
+    order5 = Order(order_id=5, side=Side.SELL, quantity=15, order_type=OrderType.LIMIT, price=103)
+    order6 = Order(order_id=6, side=Side.SELL, quantity=20, order_type=OrderType.LIMIT, price=104)
+
+    # Add these orders to the instruction queue as NEW_ORDER
+    spy_market.instruction_queue.add_instruction(order1.to_new_order_instruction())
+
+
 
 
 if __name__ == "__main__":
--- src/Order.py
+++ src/Order.py
@@ -19,24 +19,31 @@ class Order:
         self.type = order_type
         self.price = price
 
-    # Method to convert the order to a NEW_ORDER instruction
-    def to_new_order_instruction(self):
-        return Instruction(instruction_type=InstructionType.NEW_ORDER, order=self)
     
-    # Method to convert the order to a CANCEL_ORDER instruction
-    def to_cancel_order_instruction(self):
-        return Instruction(instruction_type=InstructionType.CANCEL_ORDER, order=self)
+    # Method to convert an Order to a given instruction
+    def to_instruction(self, instruction_type, new_value=None):
+        
+        # Convert to NEW_ORDER instruction
+        if instruction_type == InstructionType.NEW_ORDER:
+            return Instruction(instruction_type=InstructionType.NEW_ORDER, order=self)
+        
+        # Convert to CANCEL_ORDER instruction
+        elif instruction_type == InstructionType.CANCEL_ORDER:
+            return Instruction(instruction_type=InstructionType.CANCEL_ORDER, order=self)
+        
+        # Convert to UPDATE_PRICE instruction
+        elif instruction_type == InstructionType.UPDATE_PRICE:
+            if new_value:
+                new_order = Order(order_id=self.id, side=self.side, quantity=self.quantity, order_type=self.type, price=new_value)
+                return Instruction(instruction_type=InstructionType.UPDATE_PRICE, order=new_order)
+            else:
+                raise ValueError("New price must be provided for UPDATE_PRICE instruction")
+            
+        # Convert to UPDATE_QUANTITY instruction
+        elif instruction_type == InstructionType.UPDATE_QUANTITY:
+            if new_value:
+                new_order = Order(order_id=self.id, side=self.side, quantity=new_value, order_type=self.type, price=self.price)
+                return Instruction(instruction_type=InstructionType.UPDATE_QUANTITY, order=new_order)
+            else:
+                raise ValueError("New quantity must be provided for UPDATE_QUANTITY instruction")
     
-    # Method to convert the order to an UPDATE_PRICE instruction
-    def to_update_price_instruction(self, new_price):
-
-        # Create a copy of the order with updated price
-        new_order = Order(order_id=self.id, side=self.side, quantity=self.quantity, order_type=self.type, price=new_price)
-        return Instruction(instruction_type=InstructionType.UPDATE_PRICE, order=new_order)
-    
-    # Method to convert the order to an UPDATE_QUANTITY instruction
-    def to_update_quantity_instruction(self, new_quantity):
-
-        # Create a copy of the order with updated quantity
-        new_order = Order(order_id=self.id, side=self.side, quantity=new_quantity, order_type=self.type, price=self.price)
-        return Instruction(instruction_type=InstructionType.UPDATE_QUANTITY, order=new_order)
