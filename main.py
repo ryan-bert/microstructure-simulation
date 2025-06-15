80e28aa2f65e5791f7af04f35b7268e7d0721c26
ryan-bert <ryanberti66@gmail.com>
2025-06-15 22:31:14 +0200

    simulation working so far!!!

--- main.py
+++ main.py
@@ -20,6 +20,15 @@ def main():
     order4 = Order(order_id=4, side=Side.SELL, quantity=10, order_type=OrderType.LIMIT, price=102)
     order5 = Order(order_id=5, side=Side.SELL, quantity=15, order_type=OrderType.LIMIT, price=103)
     order6 = Order(order_id=6, side=Side.SELL, quantity=20, order_type=OrderType.LIMIT, price=104)
+    order7 = Order(order_id=7, side=Side.SELL, quantity=25, order_type=OrderType.LIMIT, price=102)
+    order8 = Order(order_id=8, side=Side.SELL, quantity=30, order_type=OrderType.LIMIT, price=103)
+    order9 = Order(order_id=9, side=Side.SELL, quantity=35, order_type=OrderType.LIMIT, price=104)
+    order10 = Order(order_id=10, side=Side.BUY, quantity=40, order_type=OrderType.LIMIT, price=101)
+    order11 = Order(order_id=11, side=Side.BUY, quantity=45, order_type=OrderType.LIMIT, price=100)
+    order12 = Order(order_id=12, side=Side.BUY, quantity=50, order_type=OrderType.LIMIT, price=99)
+
+    # Add market orders
+    order1_market = Order(order_id=13, side=Side.SELL, quantity=45, order_type=OrderType.MARKET)
 
     # Add these orders to the instruction queue as NEW_ORDER
     spy_market.instruction_queue.add_instruction(order1.to_instruction(InstructionType.NEW_ORDER))
@@ -28,16 +37,20 @@ def main():
     spy_market.instruction_queue.add_instruction(order4.to_instruction(InstructionType.NEW_ORDER))
     spy_market.instruction_queue.add_instruction(order5.to_instruction(InstructionType.NEW_ORDER))
     spy_market.instruction_queue.add_instruction(order6.to_instruction(InstructionType.NEW_ORDER))
+    spy_market.instruction_queue.add_instruction(order7.to_instruction(InstructionType.NEW_ORDER))
+    spy_market.instruction_queue.add_instruction(order8.to_instruction(InstructionType.NEW_ORDER))
+    spy_market.instruction_queue.add_instruction(order9.to_instruction(InstructionType.NEW_ORDER))
+    spy_market.instruction_queue.add_instruction(order10.to_instruction(InstructionType.NEW_ORDER))
+    spy_market.instruction_queue.add_instruction(order11.to_instruction(InstructionType.NEW_ORDER))
+    spy_market.instruction_queue.add_instruction(order12.to_instruction(InstructionType.NEW_ORDER))
+    spy_market.instruction_queue.add_instruction(order1_market.to_instruction(InstructionType.NEW_ORDER))
 
     # Process the instructions in the queue
     while not spy_market.instruction_queue.is_empty():
         spy_market.matching_engine.process_next_instruction()
 
     # Print the order book after processing
-    print("Order Book after processing instructions:")
-    print("Bids:", spy_market.order_book.bids)
-    print("Asks:", spy_market.order_book.asks)
-    print("Executed Trades:", spy_market.executed_trades)
+    spy_market.order_book.print_order_book()
 
 
 if __name__ == "__main__":
